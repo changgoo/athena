ac667b35b682d080775e1fe506f7849348407ae2
Sanghyuk Moon <smoon.richard@gmail.com>
2021-01-21 15:41:53 +0900

    Compute error in UserWorkAfterLoop
    This commit removes dependence on the hdf5 library from the
    swing amplification regression test (grav/swing.py), resolving
    https://github.com/changgoo/athena/pull/24#discussion_r561124044
    
    This commit will also change the default parameters for the msa test.

--- tst/regression/scripts/tests/grav/swing.py
+++ tst/regression/scripts/tests/grav/swing.py
@@ -5,7 +5,6 @@
 import logging
 import scripts.utils.athena as athena
 import numpy as np
-from scipy.integrate import solve_ivp
 import sys
 sys.path.insert(0, '../../vis/python')
 import athena_read  # noqa
@@ -13,7 +12,6 @@ athena_read.check_nan_flag = True
 logger = logging.getLogger('athena' + __name__[7:])  # set logger name based on module
 
 resolution_range = [32, 64]
-rate_tols = -1.99
 
 # Set dimensionless input parameters
 Lx = 1.0        # box size
@@ -23,7 +21,7 @@ Q = 2           # Toomre Q
 nJ = 2.5        # Jeans number
 nwx = -3        # number of waves in x
 nwy = 1         # number of waves in y
-amp = 1e-7      # perturbation amplitude
+amp = 1e-6      # perturbation amplitude
 
 # derived parameters
 kappa = np.sqrt(4-2*qshear)*Omega0
@@ -36,9 +34,8 @@ gconst = nJ*cs2
 
 def prepare(*args, **kwargs):
     logger.debug('Running test ' + __name__)
-    athena.configure('mpi', 'fft', 'hdf5', 'h5double',
-                     prob='msa',
-                     eos='isothermal', flux='hlle',
+    athena.configure('mpi', 'fft', prob='msa',
+                     eos='adiabatic', flux='hlle',
                      grav='blockfft', *args, **kwargs)
     athena.make()
 
@@ -46,7 +43,6 @@ def prepare(*args, **kwargs):
 def run(**kwargs):
     for n in resolution_range:
         arguments = ['job/problem_id=SwingAmplification_' + repr(n),
-                     'output1/file_type=hdf5',
                      'mesh/nx1=' + repr(n),
                      'mesh/x1min={}'.format(-Lx/2.),
                      'mesh/x1max={}'.format(Lx/2.),
@@ -62,49 +58,26 @@ def run(**kwargs):
                      'problem/nJ={}'.format(nJ),
                      'problem/amp={}'.format(amp),
                      'problem/nwx={}'.format(nwx),
-                     'problem/nwy={}'.format(nwy)]
+                     'problem/nwy={}'.format(nwy),
+                     'problem/compute_error=true']
         athena.run('mhd/athinput.msa', arguments)
 
 
 def analyze():
-    def ode(t, y, kx, ky, kappa, qshear, cs2, gconst):
-        kxt = kx + qshear*ky*t
-        kmag = np.sqrt(kxt**2+ky**2)
-        f0 = -kxt*y[1]-ky*y[2]
-        f1 = 2*y[2]+kxt*(cs2-4*np.pi*gconst/kmag**2)*y[0]
-        f2 = -0.5*kappa**2*y[1]+ky*(cs2-4*np.pi*gconst/kmag**2)*y[0]
-        return np.array([f0, f1, f2])
-
-    l2ERROR = []
-
-    # solve ODE to get the reference solution
-    y0 = 0.5*amp*np.array([1, kx/ky, 1])
-    odesol = solve_ivp(ode, (0, 2), y0, atol=1e-30, rtol=1e-13, t_eval=[2],
-                       args=[kx, ky, kappa, qshear, cs2, gconst])
-    kxt = kx+qshear*ky*odesol.t
-
-    # compute L2 norms
-    for n in resolution_range:
-        ds = athena_read.athdf('bin/SwingAmplification_' + str(n)
-                               + '.out1.00001.athdf')
-        analytic = 1.0 + 2*odesol.y[0]*np.cos(ky*ds['x2v'][None, :, None]
-                                              + kxt*ds['x1v'][None, None, :])
-        dx = Lx/n
-        l2ERROR.append(np.sqrt(((ds['rho']-analytic)**2*dx**2).sum()))
-
-    # estimate L2 convergence
+    # read data from error file
+    filename = 'bin/msa-errors.dat'
+    data = athena_read.error_dat(filename)
     analyze_status = True
-    conv = (np.diff(np.log(np.array(l2ERROR)))
-            / np.diff(np.log(np.array(resolution_range))))
-    logger.info('[Swing Amplification]: Convergence order = {}'
-                .format(conv))
 
-    if conv > rate_tols:
-        logger.warning('[Swing Amplification]: '
-                       'Scheme NOT converging at expected order.')
+    # Check absolute error and convergence rate lower bounds of all waves
+    # Asymptotic second-order convergence should have ratio <= 0.25 below
+    if data[1][4] > 2.3e-7:
+        logger.warning("error in the shearing wave too large %g",
+                       data[1][4])
         analyze_status = False
-    else:
-        logger.info('[Swing Amplification]: '
-                    'Scheme converging at expected order.')
+    if data[1][4]/data[0][4] > 0.25:
+        logger.warning("not converging for shearing wave %g %g",
+                       data[0][4], data[1][4])
+        analyze_status = False
 
     return analyze_status
