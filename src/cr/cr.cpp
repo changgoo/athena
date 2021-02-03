17aed10347d643294edce2de737e8efa05f99426
larmillo <lucia.armillotta@hotmail.it>
2021-02-02 19:41:47 -0500

    New Opacity Function 2

--- src/cr/cr.cpp
+++ src/cr/cr.cpp
@@ -188,6 +188,10 @@ inline void DefaultOpacity(MeshBlock *pmb, AthenaArray<Real> &u_cr,
 #pragma omp simd
         for(int i=il; i<=iu; ++i){
 
+          pcr->sigma_diff(0,k,j,i) = pcr->sigma;
+          pcr->sigma_diff(1,k,j,i) = pcr->max_opacity;
+          pcr->sigma_diff(2,k,j,i) = pcr->max_opacity;  
+          
           pcr->sigma_adv(0,k,j,i) = pcr->max_opacity;
           pcr->sigma_adv(1,k,j,i) = pcr->max_opacity;
           pcr->sigma_adv(2,k,j,i) = pcr->max_opacity;  
