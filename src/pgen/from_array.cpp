6b33b1ede31820685e5f63fd7d0fd6050e05ec6e
Sanghyuk Moon <smoon.richard@gmail.com>
2021-02-18 18:08:42 +0900

    Add self_gravity blocks in input files and add an input file for polytrope problem

--- src/pgen/from_array.cpp
+++ src/pgen/from_array.cpp
@@ -40,8 +40,8 @@
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
   if (SELF_GRAVITY_ENABLED) {
-    Real four_pi_G = pin->GetReal("problem","four_pi_G");
-    Real eps = pin->GetOrAddReal("problem","grav_eps", 0.0);
+    Real four_pi_G = pin->GetReal("self_gravity","four_pi_G");
+    Real eps = pin->GetOrAddReal("self_gravity","grav_eps", 0.0);
     SetFourPiG(four_pi_G);
     SetGravityThreshold(eps);
   }
