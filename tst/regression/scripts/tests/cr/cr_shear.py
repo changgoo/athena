c238658299307fa05ab36c4ab04fc6127cd0209f
larmillo <lucia.armillotta@hotmail.it>
2021-03-12 15:01:48 -0500

    Updates in the regression test for CRs with shear

--- tst/regression/scripts/tests/cr/cr_shear.py
+++ tst/regression/scripts/tests/cr/cr_shear.py
@@ -19,11 +19,11 @@ def prepare(*args, **kwargs):
 # Run Athena++
 def run(**kwargs):
     # Both streaming and diffusion and advection are on, magnetic field along x direction
-    arguments = ['mesh/nx1=512', 'mesh/ix1_bc=shear_periodic',
-                 'mesh/ox1_bc=shear_periodic', 'mesh/nx2=128',
+    arguments = ['mesh/nx1=256', 'mesh/ix1_bc=shear_periodic',
+                 'mesh/ox1_bc=shear_periodic', 'mesh/nx2=64',
                  'mesh/ix2_bc=periodic', 'mesh/ox2_bc=periodic',
-                 'meshblock/nx1=512', 'meshblock/nx2=128', 'time/tlim=0.1',
-                 'cr/vmax=200', 'cr/sigma=10', 'orbital_advection/Omega0=0.2',
+                 'meshblock/nx1=256', 'meshblock/nx2=64', 'time/tlim=0.02',
+                 'cr/vmax=200', 'cr/sigma=10', 'orbital_advection/Omega0=0.5',
                  'orbital_advection/qshear=1', 'problem/direction=0',
                  'problem/B0=1', 'problem/offset1=0.5', 'problem/cells=8']
     athena.run('cosmic_ray/athinput.cr_shear', arguments)
@@ -43,8 +43,8 @@ def analyze():
     err = np.abs(Ec-Ec_shear)
 
     # check absolute error
-    if (any(err > 3e-5)):
-        index = np.where((err) > 3e-5)[0]
+    if (any(err > 4e-6)):
+        index = np.where((err) > 4e-6)[0]
         print("error in cosmic-ray propagation =", err[index], "in x =", x[index])
         return False
 
