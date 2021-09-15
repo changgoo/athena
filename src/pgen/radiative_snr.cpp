a54695da0137b6106e50471b4f40042031d94271
Chang-Goo Kim <cgkim@astro.princeton.edu>
2021-09-15 12:55:36 -0400

    Add SNR histories

--- src/pgen/radiative_snr.cpp
+++ src/pgen/radiative_snr.cpp
@@ -35,9 +35,13 @@ Units *punit;
 // will be set to specific function depending on the input parameter (cooling/coolftn).
 CoolingFunctionBase *pcool;
 
-// explicit cooling solver using RK4 method for integration
-// slightly modified to update T*(n/n_H) rather than T itself
+// user history
 Real CoolingLosses(MeshBlock *pmb, int iout);
+Real HistoryMass(MeshBlock *pmb, int iout);
+Real HistoryRadialMomentum(MeshBlock *pmb, int iout);
+Real HistoryShell(MeshBlock *pmb, int iout);
+
+// user timestep
 static Real cooling_timestep(MeshBlock *pmb);
 
 // calculate tcool = e/L(rho, P)
@@ -49,6 +53,7 @@ void PrintCoolingFunction(CoolingFunctionBase *pcool, std::string coolftn);
 void PrintParameters(CoolingFunctionBase *pcool, const Real rho, const Real Press);
 
 Real cfl_op_cool=-1;
+Real Thot0 = 2.e4, vr0=0.1;
 //========================================================================================
 //! \fn void Mesh::InitUserMeshData(ParameterInput *pin)
 //! \brief Function to initialize problem-specific data in mesh class.  Can also be used
@@ -62,10 +67,12 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
 
   if (coolftn.compare("tigress") == 0) {
     pcool = new TigressClassic(pin);
-    std::cout << "Cooling function is set to TigressClassic" << std::endl;
+    if (Globals::my_rank == 0)
+      std::cout << "Cooling function is set to TigressClassic" << std::endl;
   } else if (coolftn.compare("plf") ==0) {
     pcool = new PiecewiseLinearFits(pin);
-    std::cout << "Cooling function is set to PiecewiseLinearFits" << std::endl;
+    if (Globals::my_rank == 0)
+      std::cout << "Cooling function is set to PiecewiseLinearFits" << std::endl;
   } else {
     std::stringstream msg;
     msg << "### FATAL ERROR in ProblemGenerator" << std::endl
@@ -97,8 +104,14 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
   EnrollUserTimeStepFunction(cooling_timestep);
 
   // Enroll user-defined functions
-  AllocateUserHistoryOutput(1);
+  AllocateUserHistoryOutput(7);
   EnrollUserHistoryOutput(0, CoolingLosses, "e_cool");
+  EnrollUserHistoryOutput(1, HistoryMass, "M");
+  EnrollUserHistoryOutput(2, HistoryMass, "M_hot");
+  EnrollUserHistoryOutput(3, HistoryRadialMomentum, "pr");
+  EnrollUserHistoryOutput(4, HistoryShell, "M_sh");
+  EnrollUserHistoryOutput(5, HistoryShell, "pr_sh");
+  EnrollUserHistoryOutput(6, HistoryShell, "RM_sh");
 }
 
 //========================================================================================
@@ -143,43 +156,43 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real rho_0   = pin->GetReal("problem", "rho_0"); // measured in m_p muH cm^-3
   Real pgas_0  = pin->GetReal("problem", "pgas_0"); // measured in kB K cm^-3
 
-  // below is for sanity check. Uncomment if needed
-  Real mu = pcool->Get_mu(rho_0, pgas_0/pcool->to_pok);
-  Real muH = pcool->Get_muH();
-  Real T = pgas_0/rho_0*(mu/muH);
-  //
-  std::cout << "============== Check Initialization ===============" << std::endl
-            << " Input (nH, P/k, T) in cgs = " << rho_0 << " " << pgas_0
-            << " " << T << std::endl
-            << "  mu = " << mu << " mu(punit) = " << punit->mu
-            << " muH = " << muH << std::endl;
-            // << " tcool = " << tcool(pcool, T, rho_0) << std::endl;
-
   rho_0 /= pcool->to_nH; // to code units
   pgas_0 /= pcool->to_pok; // to code units
 
-  PrintParameters(pcool,rho_0,pgas_0);
-  T = pcool->GetTemperature(rho_0,pgas_0);
-  Real nH = rho_0*pcool->to_nH;
-  std::cout << "  Tempearture = " << T << std::endl;
-  std::cout << " tcool = " << tcool(pcool, rho_0, pgas_0) << std::endl;
-  std::cout << " sound speed = " << std::sqrt(pgas_0/rho_0) << std::endl;
-
   // Initialize primitive values
-  Real my_vol = 0;
-  Real r_sn = pin->GetReal("problem","r_sn");
   for (int k = ks; k <= ke; ++k) {
     for (int j = js; j <= je; ++j) {
       for (int i = is; i <= ie; ++i) {
-        phydro->w(IDN,k,j,i) = rho_0;
-        phydro->w(IPR,k,j,i) = pgas_0;
-        phydro->w(IVX,k,j,i) = 0.0;
-        phydro->w(IVY,k,j,i) = 0.0;
-        phydro->w(IVZ,k,j,i) = 0.0;
-        Real r = std::sqrt(SQR(pcoord->x1v(i))
-                          +SQR(pcoord->x2v(j))
-                          +SQR(pcoord->x3v(k)));
-        if (r<r_sn) my_vol += pcoord->GetCellVolume(k,j,i);
+        phydro->u(IDN,k,j,i) = rho_0;
+        phydro->u(IEN,k,j,i) = pgas_0;
+        phydro->u(IM1,k,j,i) = 0.0;
+        phydro->u(IM2,k,j,i) = 0.0;
+        phydro->u(IM3,k,j,i) = 0.0;
+      }
+    }
+  }
+  return;
+}
+
+//========================================================================================
+//! \fn void Mesh::PostInitialize(int res_flag, ParameterInput *pin)
+//! \brief add SN energy
+//========================================================================================
+void Mesh::PostInitialize(int res_flag, ParameterInput *pin) {
+  Real my_vol = 0;
+  Real r_sn = pin->GetReal("problem","r_sn");
+  for (int b=0; b<nblocal; ++b){
+    MeshBlock *pmb = my_blocks(b);
+    // Initialize primitive values
+    for (int k = pmb->ks; k <= pmb->ke; ++k) {
+      for (int j = pmb->js; j <= pmb->je; ++j) {
+        for (int i = pmb->is; i <= pmb->ie; ++i) {
+          Real r = std::sqrt(SQR(pmb->pcoord->x1v(i))
+                            +SQR(pmb->pcoord->x2v(j))
+                            +SQR(pmb->pcoord->x3v(k)));
+          if (r<r_sn) my_vol += pmb->pcoord->GetCellVolume(k,j,i);
+
+        }
       }
     }
   }
@@ -191,26 +204,23 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
   // get pressure fron SNe in the code unit
   Real Esn = pin->GetOrAddReal("problem","Esn",1.0)*pcool->punit->Bethe_in_code;
-  Real Psn = Esn/my_vol*(peos->GetGamma()-1);
-
+  Real usn = Esn/my_vol;
+  std::cout << " SN energy: " << Esn << " volume: " << my_vol << " E/vol: " << usn << std::endl;
   // add the SN energy
-  for (int k = ks; k <= ke; ++k) {
-    for (int j = js; j <= je; ++j) {
-      for (int i = is; i <= ie; ++i) {
-        Real r = std::sqrt(SQR(pcoord->x1v(i))
-                          +SQR(pcoord->x2v(j))
-                          +SQR(pcoord->x3v(k)));
-        if (r<r_sn)
-          phydro->w(IPR,k,j,i) += Psn;
+  for (int b=0; b<nblocal; ++b){
+    MeshBlock *pmb = my_blocks(b);
+    Hydro *phydro = pmb->phydro;
+    for (int k = pmb->ks; k <= pmb->ke; ++k) {
+      for (int j = pmb->js; j <= pmb->je; ++j) {
+        for (int i = pmb->is; i <= pmb->ie; ++i) {
+          Real r = std::sqrt(SQR(pmb->pcoord->x1v(i))
+                            +SQR(pmb->pcoord->x2v(j))
+                            +SQR(pmb->pcoord->x3v(k)));
+          if (r<r_sn) phydro->u(IEN,k,j,i) += usn;
+        }
       }
     }
   }
-
-  // Initialize conserved values
-  AthenaArray<Real> b;
-  peos->PrimitiveToConserved(phydro->w, b, phydro->u, pcoord,
-       il, iu, jl, ju, kl, ku);
-  return;
 }
 
 //========================================================================================
@@ -252,40 +262,40 @@ void MeshBlock::UserWorkInLoop() {
         Real P_before = w_p; // store original P
         Real rho_before = w_d; // store original d
         Real nH_before = w_d*pcool->to_nH; // store original nH
-        Real T_before = P_before*pcool->to_pok/nH_before;
+        Real T1_before = P_before/rho_before*pcool->punit->Temperature;
         Real tnow = 0., tleft = dt_mhd;
-        Real P_next, T_next; // declare vars outside while-scope
+        Real P_next, T1_next; // declare vars outside while-scope
         int nsub=0;
         while (tnow < dt_mhd) {
           Real dt_net = cfl_op_cool*dtnet(pcool, rho_before, P_before);
           Real dt_sub = std::min(std::min(dt_mhd,dt_net),tleft);
 
-          T_next = T_before*(1-dt_sub/tcool(pcool, rho_before, P_before));
-          P_next = nH_before*T_next/pcool->to_pok;
+          T1_next = T1_before*(1-dt_sub/tcool(pcool, rho_before, P_before));
+          P_next = rho_before*T1_next/pcool->punit->Temperature;
 
           tnow += dt_sub;
           tleft = dt_mhd-tnow;
 
           P_before = P_next;
-          T_before = P_before*pcool->to_pok/nH_before;
+          T1_before = P_before/rho_before*pcool->punit->Temperature;
         }
 
         // dont cool below cooling floor and find new internal thermal energy
         Real T_floor = pcool->Get_Tfloor();
-        Real P_floor = T_floor*nH_before/pcool->to_pok;
+        Real P_floor = rho_before*T_floor/pcool->punit->Temperature;
 
         Real P_after = std::max(P_next,P_floor);
         Real u_after = P_after/(pcool->gamma_adi-1.0);
 
-        // change internal energy
-        u_e = u_after + e_non_thermal;
-        w_p = P_after;
-
         // store edot (code units)
-        Real delta_e = (P_after-P_before)/(pcool->gamma_adi-1.0);
+        Real delta_e = (P_after-w_p)/(pcool->gamma_adi-1.0);
         edot(k,j,i) = delta_e/pmy_mesh->dt;
 
         delta_e_block += delta_e;
+
+        // change internal energy
+        u_e = u_after + e_non_thermal;
+        w_p = P_after;
       }
     }
   }
@@ -365,6 +375,117 @@ Real CoolingLosses(MeshBlock *pmb, int iout) {
   pmb->ruser_meshblock_data[0](iout) = 0.0;
   return delta_e;
 }
+
+//========================================================================================
+//! \fn Real HistoryMass(MeshBlock *pmb, int iout)
+//! \brief Total gas mass (total/hot)
+//========================================================================================
+Real HistoryMass(MeshBlock *pmb, int iout) {
+  int is=pmb->is, ie=pmb->ie;
+  int js=pmb->js, je=pmb->je;
+  int ks=pmb->ks, ke=pmb->ke;
+  AthenaArray<Real> vol(pmb->ncells1);
+  AthenaArray<Real> rho, press;
+  rho.InitWithShallowSlice(pmb->phydro->u,4,IDN,1);
+  press.InitWithShallowSlice(pmb->phydro->w,4,IPR,1);
+  Real mass=0.0;
+  Real Temp;
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      pmb->pcoord->CellVolume(k, j, pmb->is, pmb->ie, vol);
+      for (int i=is; i<=ie; ++i) {
+        if (iout == 1) {
+          mass += rho(k,j,i)*vol(i);
+        } else {
+          Temp = pcool->GetTemperature(rho(k,j,i), press(k,j,i));
+          if (Temp > Thot0) {
+            mass += rho(k,j,i)*vol(i);
+          }
+        }
+      }
+    }
+  }
+  return mass;
+}
+
+//========================================================================================
+//! \fn Real HistoryMass(MeshBlock *pmb, int iout)
+//! \brief Total gas mass
+//========================================================================================
+Real HistoryRadialMomentum(MeshBlock *pmb, int iout) {
+  int is=pmb->is, ie=pmb->ie;
+  int js=pmb->js, je=pmb->je;
+  int ks=pmb->ks, ke=pmb->ke;
+  AthenaArray<Real> vol(pmb->ncells1);
+
+  Real pr = 0;
+  Real x0 = 0.0, y0 = 0.0, z0 = 0.0;
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      pmb->pcoord->CellVolume(k, j, pmb->is, pmb->ie, vol);
+      for (int i=is; i<=ie; ++i) {
+        Real x = pmb->pcoord->x1v(i);
+        Real y = pmb->pcoord->x2v(j);
+        Real z = pmb->pcoord->x3v(k);
+        Real Mx = pmb->phydro->u(IM1,k,j,i);
+        Real My = pmb->phydro->u(IM2,k,j,i);
+        Real Mz = pmb->phydro->u(IM3,k,j,i);
+        Real rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
+        pr += vol(i)*(Mx*(x-x0) + My*(y-y0) + Mz*(z-z0))/rad;
+      }
+    }
+  }
+
+  return pr;
+
+}
+
+//========================================================================================
+//! \fn Real HistoryMass(MeshBlock *pmb, int iout)
+//! \brief Total gas mass (test)
+//========================================================================================
+Real HistoryShell(MeshBlock *pmb, int iout) {
+  int is=pmb->is, ie=pmb->ie;
+  int js=pmb->js, je=pmb->je;
+  int ks=pmb->ks, ke=pmb->ke;
+  AthenaArray<Real> vol(pmb->ncells1);
+  AthenaArray<Real> rho, press;
+  rho.InitWithShallowSlice(pmb->phydro->u,4,IDN,1);
+  press.InitWithShallowSlice(pmb->phydro->w,4,IPR,1);
+
+  Real sum = 0.0;
+  Real x0 = 0.0, y0 = 0.0, z0 = 0.0;
+  Real Temp;
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      pmb->pcoord->CellVolume(k, j, pmb->is, pmb->ie, vol);
+      for (int i=is; i<=ie; ++i) {
+        Real x = pmb->pcoord->x1v(i);
+        Real y = pmb->pcoord->x2v(j);
+        Real z = pmb->pcoord->x3v(k);
+
+        Real vx = pmb->phydro->w(IVX,k,j,i);
+        Real vy = pmb->phydro->w(IVY,k,j,i);
+        Real vz = pmb->phydro->w(IVZ,k,j,i);
+        Real rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
+        Real vr = (vx*(x-x0) + vy*(y-y0) + vz*(z-z0))/rad;
+        Temp = pcool->GetTemperature(rho(k,j,i), press(k,j,i));
+        if ((Temp < Thot0) && (vr > vr0)) {
+          if (iout == 4) { // Mass
+            sum += rho(k,j,i)*vol(i);
+          } else if (iout == 5) { // Momentum
+            sum += rho(k,j,i)*vr*vol(i);
+          } else if (iout == 6) { // mass-weighted radius (need to be divided by shell mass)
+            sum += rho(k,j,i)*vol(i)*rad;
+          }
+        }
+      }
+    }
+  }
+
+  return sum;
+}
+
 
 //========================================================================================
 //! \fn void PrintCoolingFunction(CoolingFunctionBase *pcool,std::string coolftn)
