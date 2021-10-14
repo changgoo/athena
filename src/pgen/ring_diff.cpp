9e0b6dea2d00279614a933dfbbeadfb6c4d1c299
Chang-Goo Kim <cgkim@astro.princeton.edu>
2021-10-14 14:33:30 -0400

    Add diffusion of gaussian

--- src/pgen/ring_diff.cpp
+++ src/pgen/ring_diff.cpp
@@ -11,6 +11,8 @@
 //!   - For STS, simply comment out
 //!     ptlist->DoTaskListOneStage(pmesh, stage);
 //!   - For explicit diffusion, need to modify time integrator task list
+//! REFERENCE:
+//! - Parrish, I.~J. \& Stone, J.~M.\ 2005, \apj, 633, 334. doi:10.1086/444589
 //========================================================================================
 
 // C headers
@@ -35,21 +37,23 @@
 #include "../parameter_input.hpp"
 #include "../scalars/scalars.hpp"
 
-Real threshold;
-
-int RefinementCondition(MeshBlock *pmb);
+Real HistoryEleak(MeshBlock *pmb, int iout);
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
-  if (adaptive) {
-    EnrollUserRefinementCondition(RefinementCondition);
-    threshold = pin->GetReal("problem","thr");
-  }
   if (!MAGNETIC_FIELDS_ENABLED) {
     std::stringstream msg;
     msg << "### FATAL ERROR in ring_diff.cpp ProblemGenerator" << std::endl
         << "Magnetic field must be turned on" << std::endl;
     ATHENA_ERROR(msg);
   }
+  if (!NON_BAROTROPIC_EOS) {
+    std::stringstream msg;
+    msg << "### FATAL ERROR in ring_diff.cpp ProblemGenerator" << std::endl
+        << "EOS must be non barotropic" << std::endl;
+    ATHENA_ERROR(msg);
+  }
+  AllocateUserHistoryOutput(1);
+  EnrollUserHistoryOutput(0, HistoryEleak, "Eleak");
   return;
 }
 
@@ -65,47 +69,70 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real r1 = 0.5, r2 = 0.7;
   Real phi1 = PI*5/12., phi2 = PI*7/12.;
   Real d0 = 1.;
-  int iprob = pin->GetOrAddInteger("problem", "iprob", 0);
+  int iprob = pin->GetOrAddInteger("problem", "iprob", 1);
+  Real amp = pin->GetOrAddReal("problem", "amp", 1.e-6);
+  Real t0 = pin->GetOrAddReal("problem", "t0", 0.5);
+  Real kappa = pin->GetOrAddReal("problem", "kappa_iso", 0.);
+  kappa += pin->GetOrAddReal("problem", "kappa_aniso", 1);
+  Real x10 = pin->GetOrAddReal("problem", "x10", 0.);
+  Real x20 = pin->GetOrAddReal("problem", "x20", 0.6);
+  Real x30 = pin->GetOrAddReal("problem", "x30", 0.);
 
-  if (iprob == 1) { // 2-D diffusion
-    for (int k=ks; k<=ke; ++k) {
-      for (int j=js; j<=je; ++j) {
-        for (int i=is; i<=ie; ++i) {
-          if (std::strcmp(COORDINATE_SYSTEM, "cartesian") == 0) {
-            x1 = pcoord->x1v(i);
-            x2 = pcoord->x2v(j);
-            r = std::sqrt(SQR(x1)+SQR(x2));
-            phi = std::atan2(x2,x1);
-          } else if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {
-            r = pcoord->x1v(i);
-            phi = pcoord->x2v(j);
-            x1 = r*std::cos(phi);
-            x2 = r*std::sin(phi);
-          } else {
-            std::stringstream msg;
-            msg << "### FATAL ERROR in ring_diff.cpp ProblemGenerator" << std::endl
-                << "2-d diffusion test only compatible with cartesian or"
-                << std::endl << "cylindrical coord" << std::endl;
-            ATHENA_ERROR(msg);
-          }
-          phydro->w(IDN,k,j,i) = d0;
+  // set a uniform, static medium
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      for (int i=is; i<=ie; ++i) {
+        phydro->w(IDN,k,j,i) = d0;
+        phydro->w(IVX,k,j,i) = v1;
+        phydro->w(IVY,k,j,i) = v2;
+        phydro->w(IVZ,k,j,i) = v3;
+      }
+    }
+  }
 
-          phydro->w(IVX,k,j,i) = v1;
-          phydro->w(IVY,k,j,i) = v2;
-          phydro->w(IVZ,k,j,i) = v3;
-          if (NON_BAROTROPIC_EOS) {
-            if ((r>r1) & (r<r2) & (phi>phi1) & (phi<phi2))
-              phydro->w(IPR,k,j,i) = 12;
-            else
-              phydro->w(IPR,k,j,i) = 10;
-          }
+  // set thermal energy to diffuse
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      for (int i=is; i<=ie; ++i) {
+        if (std::strcmp(COORDINATE_SYSTEM, "cartesian") == 0) {
+          x1 = pcoord->x1v(i);
+          x2 = pcoord->x2v(j);
+          x3 = pcoord->x3v(j);
+          r = std::sqrt(SQR(x1)+SQR(x2));
+          phi = std::atan2(x2,x1);
+        } else if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {
+          r = pcoord->x1v(i);
+          phi = pcoord->x2v(j);
+          x1 = r*std::cos(phi);
+          x2 = r*std::sin(phi);
+          x3 = pcoord->x3v(j);
+        } else {
+          std::stringstream msg;
+          msg << "### FATAL ERROR in ring_diff.cpp ProblemGenerator" << std::endl
+              << "2-d diffusion test only compatible with cartesian or"
+              << std::endl << "cylindrical coord" << std::endl;
+          ATHENA_ERROR(msg);
+        }
+        if (iprob == 1) { // in-plane, ring diffusion
+          if ((r>r1) & (r<r2) & (phi>phi1) & (phi<phi2))
+            phydro->w(IPR,k,j,i) = 12;
+          else
+            phydro->w(IPR,k,j,i) = 10;
+        } else if (iprob == 2) { //gaussian diffusion
+          Real rsq = SQR(x1-x10);
+          if (je > js) rsq += SQR(x2-x20);
+          if (ke > ks) rsq += SQR(x3-x30);
+          phydro->w(IPR,k,j,i) = amp/std::pow(std::sqrt(4.*PI*kappa*t0),pmy_mesh->ndim)
+                                  * std::exp(-rsq/(4.*kappa*t0));
+        } else {
+          std::stringstream msg;
+          msg << "### FATAL ERROR in ring_diff.cpp ProblemGenerator" << std::endl
+              << "unsupported iprob = " << iprob << std::endl;
+          ATHENA_ERROR(msg);
         }
       }
     }
-  } else if (iprob == 2) {
-
   }
-
 
   // magnetic fields
   // circular in XY plane
@@ -168,45 +195,35 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   return;
 }
 
-// refinement condition: check the maximum scalar gradient
-int RefinementCondition(MeshBlock *pmb) {
-  int f2 = pmb->pmy_mesh->f2, f3 = pmb->pmy_mesh->f3;
-  AthenaArray<Real> &r = pmb->pscalars->r;
-  Real maxeps = 0.;
-  if (NSCALARS > 0) {
-    if (f3) {
-      for (int k=pmb->ks-1; k<=pmb->ke+1; k++) {
-        for (int j=pmb->js-1; j<=pmb->je+1; j++) {
-          for (int i=pmb->is-1; i<=pmb->ie+1; i++) {
-            Real eps = std::sqrt(SQR(0.5*(r(0,k,j,i+1) - r(0,k,j,i-1)))
-                                 + SQR(0.5*(r(0,k,j+1,i) - r(0,k,j-1,i)))
-                                 + SQR(0.5*(r(0,k+1,j,i) - r(0,k-1,j,i))));
-            maxeps = std::max(maxeps, eps);
-          }
+Real HistoryEleak(MeshBlock *pmb, int iout) {
+  Real x1, x2;
+  Real r, phi;
+  Real r1=0.5, r2=0.7;
+  Real Eleak = 0.0;
+  int is = pmb->is, ie = pmb->ie, js = pmb->js, je = pmb->je, ks = pmb->ks, ke = pmb->ke;
+  AthenaArray<Real> &w = pmb->phydro->w;
+  AthenaArray<Real> volume; // 1D array of volumes
+  // allocate 1D array for cell volume used in usr def history
+  volume.NewAthenaArray(pmb->ncells1);
+
+  for (int k=ks; k<=ke; k++) {
+    for (int j=js; j<=je; j++) {
+      pmb->pcoord->CellVolume(k, j, is, ie, volume);
+      for (int i=is; i<=ie; i++) {
+        if (std::strcmp(COORDINATE_SYSTEM, "cartesian") == 0) {
+          x1 = pmb->pcoord->x1v(i);
+          x2 = pmb->pcoord->x2v(j);
+          r = std::sqrt(SQR(x1)+SQR(x2));
+          phi = std::atan2(x2,x1);
+        } else if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {
+          r = pmb->pcoord->x1v(i);
+          phi = pmb->pcoord->x2v(j);
+          x1 = r*std::cos(phi);
+          x2 = r*std::sin(phi);
         }
-      }
-    } else if (f2) {
-      int k = pmb->ks;
-      for (int j=pmb->js-1; j<=pmb->je+1; j++) {
-        for (int i=pmb->is-1; i<=pmb->ie+1; i++) {
-          Real eps = std::sqrt(SQR(0.5*(r(0,k,j,i+1) - r(0,k,j,i-1)))
-                               + SQR(0.5*(r(0,k,j+1,i) - r(0,k,j-1,i))));
-          maxeps = std::max(maxeps, eps);
-        }
-      }
-    } else {
-      int k = pmb->ks;
-      int j = pmb->js;
-      for (int i=pmb->is-1; i<=pmb->ie+1; i++) {
-        Real eps = std::sqrt(SQR(0.5*(r(0,k,j,i+1) - r(0,k,j,i-1))));
-        maxeps = std::max(maxeps, eps);
+        if ((r<r1) | (r>r2)) Eleak += (w(IPR,k,j,i)-10)*volume(i);
       }
     }
-  } else {
-    return 0;
   }
-
-  if (maxeps > threshold) return 1;
-  if (maxeps < 0.25*threshold) return -1;
-  return 0;
+  return Eleak;
 }
