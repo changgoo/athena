ea17a84b86ce19a6f88ed080f949fbef5b267de3
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-01-06 14:33:54 -0500

    Fix wrong rebase conflict resolution at 66000fc0

--- src/particles/particles.cpp
+++ src/particles/particles.cpp
@@ -175,7 +175,7 @@ Particles::Particles(MeshBlock *pmb, ParameterInput *pin, ParticleParameters *pp
   ipid(-1), imass(-1), ixp(-1), iyp(-1), izp(-1), ivpx(-1), ivpy(-1), ivpz(-1),
   ixp0(-1), iyp0(-1), izp0(-1), ivpx0(-1), ivpy0(-1), ivpz0(-1),
   ixi1(-1), ixi2(-1), ixi3(-1), igx(-1), igy(-1), igz(-1), ish(-1),
-  npar_(0), nparmax_(1), parhstout_(false), isgravity_(false) {
+  npar_(0), nparmax_(1), parhstout_(false), isgravity_(pp->gravity) {
 
   // Add particle ID.
   ipid = AddIntProperty();
