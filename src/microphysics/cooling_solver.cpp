a5bf2ae481b228d48d18fa95488bf290b26a87eb
Chang-Goo Kim <cgkim@astro.princeton.edu>
2023-01-17 16:46:00 -0500

    Throw error for nan values at the beginning of cooling solver

--- src/microphysics/cooling_solver.cpp
+++ src/microphysics/cooling_solver.cpp
@@ -253,6 +253,15 @@ void CoolingSolver::OperatorSplitSolver(MeshBlock *pmb) {
 
         Real& w_d  = pmb->phydro->w(IDN,k,j,i);
         Real& w_p  = pmb->phydro->w(IPR,k,j,i);
+
+        if ((w_d != w_d) || (w_p != w_p)) {
+          std::stringstream msg;
+          msg << "### FATAL ERROR in OperatorSplitSolver" << std::endl
+              << "nan values have passed from integrator "
+              << "d = " << w_d << " p = " << w_p << std::endl;
+          ATHENA_ERROR(msg);
+        }
+
         // find non-thermal part of energy to keep it the same
         Real e_non_thermal = u_e - w_p*igm1;
 
