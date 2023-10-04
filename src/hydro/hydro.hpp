c2b9f2e3566ff3016147a11c9612f25e247a7a40
Chang-Goo Kim <cgkim@astro.princeton.edu>
2023-10-04 15:12:20 -0400

    Added functions for single cell diffuse flux

--- src/hydro/hydro.hpp
+++ src/hydro/hydro.hpp
@@ -127,6 +127,7 @@ class Hydro {
   TimeStepFunc UserTimeStep_;
 
   void AddDiffusionFluxes();
+  void AddDiffusionFluxesSingleCell(int i, int j, int k);
   Real GetWeightForCT(Real dflx, Real rhol, Real rhor, Real dx, Real dt);
 };
 #endif // HYDRO_HYDRO_HPP_
